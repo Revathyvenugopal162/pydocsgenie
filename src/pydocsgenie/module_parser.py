b78f2d423ab2f9743c9088e6446a1a8e741c8803
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2023-02-09 14:22:26 +0100

    Add methods

--- src/pydocsgenie/module_parser.py
+++ src/pydocsgenie/module_parser.py
@@ -1,5 +1,4 @@
-"""Module containing functions for splitting python files into its methods
-This splitting will help feeding the OpenAI model."""
+"""Module containing functions for splitting python files into its methods."""
 
 def module2class_split(module_str: str):
     """Splits a module into its component classes
@@ -14,13 +13,11 @@ def module2class_split(module_str: str):
     List[str]
         List with each of the classes of the module in string format.
     """
-    classes_splitted = module_str.split("class ")
-    # first element will be whatever appears before the class, we can ignore it.
-    classes_list = ["class " + elem for elem in classes_splitted[1:]]
+    classes_list = ["class " + elem for elem in module_str.split("class ")[1:]]
     return classes_list
 
 
-def class2methods_split(class_str: str):
+def split_methods(class_str: str):
     """Splits a class into its component methods.
 
     Parameters
@@ -33,9 +30,7 @@ def class2methods_split(class_str: str):
     List
         List with each of the methods of the class in string format.
     """
-    methods_splitted = class_str.split("def ")
-    # first element will be whatever appears before the first method, we can ignore it.
-    methods_list = ["def " + elem for elem in methods_splitted[1:]]
+    methods_list = ["def " + elem for elem in class_str.split("def ")[1:]]
     return methods_list
 
 def get_methods_from_module(module_str: str):
@@ -58,7 +53,9 @@ def get_methods_from_module(module_str: str):
     >>> methods_list = get_methods_from_module(module_str=module_str)
     """
     classes = module2class_split(module_str=module_str)
-    methods_nested = [class2methods_split(class_str=class_str) for class_str in classes]
+    if classes is None:
+        methods_nested = split_methods(class_str=module_str)
+    methods_nested = [split_methods(class_str=class_str) for class_str in classes]
 
     # flattens the list. methods_nested format: [class1[method1, method2..], class2[method1, method2..]]
     methods_flatten = [method for class_methods in methods_nested for method in class_methods]
