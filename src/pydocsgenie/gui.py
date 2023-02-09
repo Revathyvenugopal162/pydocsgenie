ad6fab922d076450e557420eca9ff67a78afe71d
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2023-02-09 20:43:48 +0100

    Add clear button

--- src/pydocsgenie/gui.py
+++ src/pydocsgenie/gui.py
@@ -1,11 +1,8 @@
-
-# importing libraries
-import sys
-
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from inference import inference
+import sys
 
 
 class Window(QMainWindow):
@@ -28,19 +25,34 @@ class Window(QMainWindow):
     def UiComponents(self):
  
         # creating a push button
-        button = QPushButton("Get Docstrings", self)
+        generate_button = QPushButton("Generate Docstring", self)
+        clear_button = QPushButton("Clear", self)
         self.textbox_input = QLineEdit(self)
         self.textbox_output = QTextBrowser(self)
         # setting geometry of button
-        button.setGeometry(200, 30, 200, 40)
+        generate_button.setGeometry(200, 30, 200, 40)
+        clear_button.setGeometry(400, 30, 200, 40)
         self.textbox_input.setGeometry(20, 100, 200, 200)
         self.textbox_output.setGeometry(320, 100, 200, 200)
 
-        # adding action to a button
-        button.clicked.connect(self.clickme)
+        # adding action to buttons
+        generate_button.clicked.connect(self.generate_docstring)
+        clear_button.clicked.connect(self.clear_output)
 
-    # action method
-    def clickme(self):
+    # action method for generating docstring
+    def generate_docstring(self):
         input_text = self.textbox_input.text()     
         output = inference(input_text)
         self.textbox_output.append(output)
+
+    # action method for clearing output
+    def clear_output(self):
+        self.textbox_output.clear()
+
+App = QApplication(sys.argv)
+ 
+# create the instance of our Window
+window = Window()
+ 
+# start the app
+sys.exit(App.exec())
